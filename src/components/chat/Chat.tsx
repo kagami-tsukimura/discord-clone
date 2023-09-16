1c7294b4b5e2c00b5def4942ea78bb797265e031
Kagami <shimura.kyohei7617@gmail.com>
2023-09-16 23:46:00 +0900

    style: chat input template

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,3 +1,7 @@
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import CardGiftcardIcon from '@mui/icons-material/CardGiftcard';
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
+import GifIcon from '@mui/icons-material/Gif';
 import './Chat.scss';
 import ChatHeader from './ChatHeader';
 const Chat = () => {
@@ -8,7 +12,20 @@ const Chat = () => {
       {/* chatMessage */}
       <div className='chatMessage'></div>
       {/* chatInput */}
-      <div className='chatInput'></div>
+      <div className='chatInput'>
+        <AddCircleOutlineIcon />
+        <form>
+          <input type='text' placeholder='#Tempへメッセージを送信' />
+          <button type='submit' className='chatInputButton'>
+            送信
+          </button>
+        </form>
+        <div className='chatInputIcons'>
+          <CardGiftcardIcon />
+          <GifIcon />
+          <EmojiEmotionsIcon />
+        </div>
+      </div>
     </div>
   );
 };
