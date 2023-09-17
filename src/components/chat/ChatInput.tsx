ec74f0e20faa1f4020a9e43e795efc5151aab044
Kagami <shimura.kyohei7617@gmail.com>
2023-09-17 19:09:54 +0900

    update: get input message

--- src/components/chat/ChatInput.tsx
+++ src/components/chat/ChatInput.tsx
@@ -2,14 +2,22 @@ import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import CardGiftcardIcon from '@mui/icons-material/CardGiftcard';
 import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
 import GifIcon from '@mui/icons-material/Gif';
+import { useState } from 'react';
 import './ChatInput.scss';
 
 const ChatInput = () => {
+  const [inputText, setInputText] = useState<string>('');
   return (
     <div className='chatInput'>
       <AddCircleOutlineIcon />
       <form>
-        <input type='text' placeholder='#Tempへメッセージを送信' />
+        <input
+          type='text'
+          placeholder='#Tempへメッセージを送信'
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setInputText(e.target.value)
+          }
+        />
         <button type='submit' className='chatInputButton'>
           送信
         </button>
