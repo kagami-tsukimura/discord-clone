8088e0ff11914339d6fd34bb44576fa97f49d928
Kagami <shimura.kyohei7617@gmail.com>
2023-09-17 20:34:57 +0900

    update: firestore send message

--- src/components/chat/ChatInput.tsx
+++ src/components/chat/ChatInput.tsx
@@ -2,11 +2,46 @@ import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import CardGiftcardIcon from '@mui/icons-material/CardGiftcard';
 import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
 import GifIcon from '@mui/icons-material/Gif';
+import {
+  CollectionReference,
+  DocumentData,
+  DocumentReference,
+  addDoc,
+  collection,
+  serverTimestamp,
+} from 'firebase/firestore';
 import { useState } from 'react';
+import { useAppSelector } from '../../app/hooks';
+import { db } from '../../firebase';
 import './ChatInput.scss';
 
 const ChatInput = () => {
   const [inputText, setInputText] = useState<string>('');
+  const channelId = useAppSelector((state) => state.channel.channelId);
+  const user = useAppSelector((state) => state.user.user);
+  const sendMessage = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+
+    // channels > messages > メッセージ情報
+    const collectionRef: CollectionReference<DocumentData> = collection(
+      db,
+      'channels',
+      String(channelId),
+      'messages'
+    );
+
+    const docRef: DocumentReference<DocumentData> = await addDoc(
+      collectionRef,
+      {
+        message: inputText,
+        timestamp: serverTimestamp(),
+        user: user,
+      }
+    );
+    console.log(docRef);
+  };
   return (
     <div className='chatInput'>
       <AddCircleOutlineIcon />
@@ -18,7 +53,13 @@ const ChatInput = () => {
             setInputText(e.target.value)
           }
         />
-        <button type='submit' className='chatInputButton'>
+        <button
+          type='submit'
+          className='chatInputButton'
+          onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) =>
+            sendMessage(e)
+          }
+        >
           送信
         </button>
       </form>
